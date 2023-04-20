4d63eb8a1200aad04a11a237943333030358ce5c
Sebastian König <s.koenig@kellerkinder.de>
2023-04-20 20:42:37 +0200

    feat: add loading method to preload the preview image

--- Resources/app/storefront/src/thumbhashloader.plugin.js
+++ Resources/app/storefront/src/thumbhashloader.plugin.js
@@ -10,17 +10,44 @@ export default class ThumbhashloaderPlugin extends Plugin {
             return;
         }
 
-        this.el.src = this.getImage(thumbhashstring);
+        this.load(this.el, thumbhashstring);
 
         this.el.dataset.thumbhash = '';
     }
 
-    getImage(thumbhashstring) {
+    getImageFromThumbHashString(thumbhashstring) {
         const hash = this.convertStringToHash(thumbhashstring);
         return thumbhash.thumbHashToDataURL(hash);
     }
 
     convertStringToHash(str) {
         return Array.from(atob(str)).map(c => c.charCodeAt(0))
+    }
+
+    load(image, thumbhashstring) {
+        const img = new Image();
+        img.onload = function () {
+            image.src = img.src;
+            img.onload = null;
+            image.style.aspectRatio = image.getAttribute('width') + '/' + image.getAttribute('height');
+
+            //TODO: remove timeout, it's just for presentation
+            setTimeout(
+                function() {
+                    if (image.dataset.src) {
+                        image.dataset.src = '';
+                        image.src = image.dataset.src;
+                    }
+                    if (image.dataset.srcset) {
+                        image.srcset = image.dataset.srcset;
+                        image.dataset.srcset = '';
+                    }
+
+                    image.classList.remove('thumbhashloader');
+                    image.classList.add('thumbhashloaded');
+            }, 2000);
+        }
+        img.src = this.getImageFromThumbHashString(thumbhashstring);
+        image.dataset.src = img.src;
     }
 }
